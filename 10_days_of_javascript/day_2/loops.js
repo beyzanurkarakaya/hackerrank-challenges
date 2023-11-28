f715f218c77d235ae6cb955a9544bcaeee659d9f
Beyza Nur Karakaya <beyzaanur.karakaya@gmail.com>
2023-11-28 21:34:11 +0300

    loops.js added

--- 10_days_of_javascript/day_2/loops.js
+++ 10_days_of_javascript/day_2/loops.js
@@ -27,7 +27,17 @@ function readLine() {
  * Print your output using 'console.log()'.
  */
 function vowelsAndConsonants(s) {
-    
+    let vowels = ["a", "e", "i", "o", "u"]
+    for (let i = 0; i < s.length; i++) {
+        if (vowels.indexOf(s[i]) > -1) {
+            console.log(s[i])
+        }
+    }
+    for (let j = 0; j < s.length; j++){
+        if (vowels.indexOf(s[j]) == -1) {
+            console.log(s[j])
+        }
+    }
 }
 
 
