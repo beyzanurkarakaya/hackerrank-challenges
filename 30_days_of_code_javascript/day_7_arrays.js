e5023470406c16fc2e6d42609d2595f5ba2ff271
Beyza Nur Karakaya <beyzaanur.karakaya@gmail.com>
2023-12-04 15:25:58 +0300

    day_7_arrays.js added

---
+++ 30_days_of_code_javascript/day_7_arrays.js
@@ -0,0 +1,33 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    
+    let copyArr = [...arr]
+    let reverseArr = (copyArr.reverse()).join(" ")
+    console.log(reverseArr.toString())
+}
