f35fbbd870c718d1df99cf8d8ab533514f0c8fd7
Beyza Nur Karakaya <beyzaanur.karakaya@gmail.com>
2023-12-12 20:53:22 +0300

    mini-max_sum.js added

---
+++ algorithms/warmup/mini-max_sum.js
@@ -0,0 +1,43 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function miniMaxSum(arr) {
+    // Write your code here
+    arr = arr.sort((a, b) => a - b)
+    let min = arr.slice(0, arr.length - 1).reduce((a, b) => a + b)
+    let max = arr.slice(1, arr.length).reduce((a, b) => a + b)
+
+    console.log(min + ' ' + max)
+}
+
+function main() {
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    miniMaxSum(arr);
+}
