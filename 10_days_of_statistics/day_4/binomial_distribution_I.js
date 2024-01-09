851ef6ab9f0988854974a1a3e8bb69296a819a11
Beyza Nur Karakaya <beyzaanur.karakaya@gmail.com>
2024-01-09 22:59:03 +0300

    binomial_distribution_I.js added

---
+++ 10_days_of_statistics/day_4/binomial_distribution_I.js
@@ -0,0 +1,45 @@
+function processData(input) {
+    //Enter your code here
+    const processInput = input.split(' ')
+    const ratio = 1.09
+    const numPerBirth = 1
+    const n = 6
+    const p = ratio / (1 + ratio)
+    let totalResult = 0
+    
+    for (let x = 3; x <= n; x++) {
+        totalResult += binomial(n, x, p)
+    }
+
+    console.log(parseFloat(totalResult).toFixed(3))
+}
+
+function factorial(n) {
+    if (n < 0) {
+        return null
+    }
+    if (n == 0 || n == 1) {
+        return 1
+    }
+    let result = 1
+    while (n > 0) {
+        result *= n--
+    }
+    return result
+}
+
+function binomial(n, x, p) {
+    let factorialResult = factorial(n) / (factorial(x) * factorial(n - x))
+    return factorialResult * Math.pow(p, x) * Math.pow(1 - p, n - x)
+}
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
