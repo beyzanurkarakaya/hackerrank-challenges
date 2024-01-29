45cbb094e01b4ba7869961203f9c813353deba26
Beyza Nur Karakaya <beyzaanur.karakaya@gmail.com>
2024-01-29 13:57:30 +0300

    minimum_distances.js added

---
+++ algorithms/implementation/minimum_distances.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'minimumDistances' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+function minimumDistances(a) {
+    // Write your code here
+    const unique = a.filter((value, index, array) => array.indexOf(value) === index)
+    let minimum = -1
+
+    unique.forEach(element => {
+        let i = a.indexOf(element)
+        let j = a.indexOf(element, i + 1)
+
+        if (j !== -1) {
+        let distance = j - i
+        if (distance < minimum || minimum === -1) {
+            minimum = distance
+        }
+        }
+    })
+    return minimum;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const result = minimumDistances(a);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
