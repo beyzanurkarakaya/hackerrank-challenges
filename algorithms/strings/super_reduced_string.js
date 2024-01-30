8588071c540411d5627d801d9dd832163c5ce018
Beyza Nur Karakaya <beyzaanur.karakaya@gmail.com>
2024-01-31 01:07:19 +0300

    super_reduced_string.js added

---
+++ algorithms/strings/super_reduced_string.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'superReducedString' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+function superReducedString(s) {
+    // Write your code here
+    let final = []
+    for(let i=0; i < s.length; i++) {
+      if(final.length != 0 && final[final.length-1] == s[i]) {
+        final.pop()
+      } else {
+        final.push(s[i])  
+      }
+      
+    }
+    return (final == "") ? "Empty String" : final.join("")
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    const result = superReducedString(s);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
