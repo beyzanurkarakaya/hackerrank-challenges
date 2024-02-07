bda45a4ea850b1ae4b6e69774e1badfd1d436b67
Beyza Nur Karakaya <beyzaanur.karakaya@gmail.com>
2024-02-07 15:25:53 +0300

    extraLongFactorials.js added

---
+++ algorithms/implementation/extraLongFactorials.js
@@ -0,0 +1,42 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'extraLongFactorials' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+function extraLongFactorials(n) {
+    // Write your code here
+    let ans = BigInt(1)
+    for(let i = n; i >= 2; i--) {
+        ans = BigInt(i) * ans
+    }
+    console.log(ans.toString())
+}
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    extraLongFactorials(n);
+}
