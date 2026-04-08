5ff46a6f67f01aea2a40aaa52051e1e92331c32e
Beyza Nur Karakaya <beyzaanur.karakaya@gmail.com>
2026-04-08 13:13:08 +0300

    update pythonIfElse.py

--- python/introduction/pythonIfElse.py
+++ python/introduction/pythonIfElse.py
@@ -10,3 +10,12 @@ import sys
 
 if __name__ == '__main__':
     n = int(input().strip())
+
+    if n%2 == 1:
+        print("Weird")
+    elif n%2 == 0 and (2<=n and n<=5):
+        print("Not Weird")
+    elif n%2 == 0 and (6<=n and n<=20):
+        print("Weird")
+    elif 20<n:
+        print("Not Weird")
